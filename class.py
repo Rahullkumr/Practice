3aeafe1f36a86ac5390b2c762e50405a16a37345
Raj Verma <91901969+rajmonu@users.noreply.github.com>
2023-12-14 18:16:01 +0000

    third night of coding practice

--- class.py
+++ class.py
@@ -1,5 +1,21 @@
-class Rahul:
-    def __init__(self):
-        print("tydjyj")
+class Test:
+    def f1(self,b):
+        self.a = 10
+        self.b = b
+        print("abc")
 
-a=Rahul()
+    @staticmethod
+    def f2():
+        print("static method")
+    
+    @classmethod
+    def f3(palpal):
+        palpal.x = 5
+        print('class method')
+
+def fun():
+    print("non member fn")
+
+t1 = Test()
+t1.b = 11
+t1.f1(5)
